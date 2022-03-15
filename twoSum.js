e9714859a01c63a6a47d617b63ec5122b90eef8a
Ibnul Ashir <ibnul.mistrisolutions@gmail.com>
2022-03-15 13:53:07 +0600

    two sum using MAP()

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+// 1. Two Sum
+
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let num1 = nums[i];
+    let num2 = target - num1;
+    if (map.has(num2)) {
+      return [i, map.get(num2)];
+    }
+    map.set(num1, i);
+  }
+};
+
+console.log(twoSum([2, 7, 11], 9));
+console.log(twoSum([2, 3, 5], 8));
