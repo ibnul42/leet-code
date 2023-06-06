5a2f0394c160325d3d7265541c0ee28f4136de90
Ibnul Ashir <ibnulashir42@gmail.com>
2023-06-06 09:51:19 +0600

    greatest common divisor

---
+++ gcdOfStrings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) {
+    return ""
+  }
+
+  const gcd = (a, b) => {
+    if (b === 0) {
+      return a
+    }
+    return gcd(b, a % b)
+  }
+
+  const length = gcd(str1.length, str2.length)
+  return str1.substring(0, length)
+}
+
+console.log(gcdOfStrings("ABCABC", "")) // Output: "ABC"
+console.log(gcdOfStrings("ABABAB", "ABAB")) // Output: "AB"
+console.log(gcdOfStrings("LEET", "CODE"))
