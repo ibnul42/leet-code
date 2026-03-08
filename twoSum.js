13d4511bcf497ef2cb188eac4a535b7e75e9fa87
Ibnul Ashir <ibnulashir42@gmail.com>
2026-03-08 12:39:05 +0600

    leetcode: modify two sum

--- twoSum.js
+++ twoSum.js
@@ -1,26 +1,12 @@
-// 1. Two Sum
-
 var twoSum = function (nums, target) {
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    let num1 = nums[i];
-    let num2 = target - num1;
-    if (map.has(num2)) {
-      return [i, map.get(num2)];
+    const targetNum = target - nums[i];
+    if (map.has(targetNum)) {
+      return [i, map.get(targetNum)];
     }
-    map.set(num1, i);
-    console.log(map);
+    map.set(nums[i], i);
   }
 };
-
-// function twoSum(arr, result) {
-//   for (let i = 0; i < arr.length - 1; i++) {
-//     if (arr[i] + arr[i + 1] === result) {
-//       console.log(arr[i], arr[i + 1]);
-//     }
-//     i++;
-//   }
-// }
-// twoSum([2, 11, 10, 20], 31);
 console.log(twoSum([2, 7, 11], 9));
 console.log(twoSum([2, 3, 4, 5], 8));
