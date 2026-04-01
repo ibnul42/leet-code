d5c3ee2376f440d72b2463bd67672205d23d6830
Ibnul Ashir <ibnulashir42@gmail.com>
2026-04-01 13:49:57 +0600

    leetcode: rotate array

--- rotateArray.js
+++ rotateArray.js
@@ -4,16 +4,24 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  while (k > 0) {
-    const s = nums.pop();
-    nums.unshift(s);
-    k--;
+  const n = nums.length;
+  k = k % n;
+
+  function reverse(left, right) {
+    while (left < right) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left++;
+      right--;
+    }
   }
+  reverse(0, n - 1);
+  reverse(0, k - 1);
+  reverse(n - k - 1, n - 1);
+
   return nums;
 };
 
 // Example usage:
-
 const nums = [1, 2, 3, 4, 5, 6, 7],
   k = 3;
 console.log(rotate(nums, k));
